28967c30d2a9363c9c1f3967575820b993780442
Billy Woods <billy.woods109@gmail.com>
2020-10-28 02:04:50 +1100

    Make DP one state earlier in GPIF waveform

--- fx2fw/fx2sdr/gpif_waveforms_simple.c
+++ fx2fw/fx2sdr/gpif_waveforms_simple.c
@@ -100,16 +100,16 @@
 //          _________ _________ _________ _________ _________ _________ _________ _________
 //                                                                                         
 // AddrMode Same Val  Same Val  Same Val  Same Val  Same Val  Same Val  Same Val           
-// DataMode NO Data   Activate  Activate  Activate  NO Data   NO Data   NO Data            
+// DataMode NO Data   Activate  Activate  Activate  Activate  Activate  Activate           
 // NextData SameData  SameData  SameData  SameData  SameData  SameData  SameData           
 // Int Trig Trig Int  No Int    No Int    No Int    No Int    No Int    No Int             
-// IF/Wait  Wait 3    Wait 11   Wait 1    Wait 1    IF        Wait 1    Wait 1             
-//   Term A                                         TCXpire                                
-//   LFunc                                          AND                                    
-//   Term B                                         TCXpire                                
-// Branch1                                          ThenIdle                               
-// Branch0                                          Else 0                                 
-// Re-Exec                                          Yes                                    
+// IF/Wait  Wait 3    Wait 11   Wait 1    IF        Wait 1    Wait 1    Wait 1             
+//   Term A                               TCXpire                                          
+//   LFunc                                AND                                              
+//   Term B                               TCXpire                                          
+// Branch1                                ThenIdle                                         
+// Branch0                                Else 0                                           
+// Re-Exec                                Yes                                              
 // Sngl/CRC Default   Default   Default   Default   Default   Default   Default            
 // nCS          0         0         1         1         1         1         1         1    
 // dummy        0         0         0         0         0         0         0         0    
@@ -169,10 +169,10 @@ const char xdata WaveData[128] =
 /* Output*/ 0x01,     0x01,     0x01,     0x01,     0x01,     0x01,     0x01,     0x01,
 /* LFun  */ 0x00,     0x00,     0x00,     0x00,     0x00,     0x00,     0x00,     0x3F,
 // Wave 2 
-/* LenBr */ 0x03,     0x0B,     0x01,     0x01,     0xB8,     0x01,     0x01,     0x07,
-/* Opcode*/ 0x10,     0x02,     0x02,     0x02,     0x01,     0x00,     0x00,     0x00,
+/* LenBr */ 0x03,     0x0B,     0x01,     0xB8,     0x01,     0x01,     0x01,     0x07,
+/* Opcode*/ 0x10,     0x02,     0x02,     0x03,     0x02,     0x02,     0x02,     0x00,
 /* Output*/ 0x00,     0x00,     0x01,     0x01,     0x01,     0x01,     0x01,     0x01,
-/* LFun  */ 0x00,     0x00,     0x00,     0x00,     0x2D,     0x00,     0x00,     0x3F,
+/* LFun  */ 0x00,     0x00,     0x00,     0x2D,     0x00,     0x00,     0x00,     0x3F,
 // Wave 3 
 /* LenBr */ 0x01,     0x01,     0x01,     0x01,     0x01,     0x01,     0x01,     0x07,
 /* Opcode*/ 0x00,     0x00,     0x00,     0x00,     0x00,     0x00,     0x00,     0x00,
